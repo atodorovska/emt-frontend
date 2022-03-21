e29965a9343e0457ca4713d553d7523f0add7701
atodorovska <ana5todorovska@gmail.com>
2022-03-21 14:58:59 +0100

    connection changes

--- src/custom-axios/axios.js
+++ src/custom-axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:9091/api',
+    baseURL: 'https://fcse-emt-backend.herokuapp.com/api',
     headers: {
         'Access-Control-Allow-Origin' : '*'
     }
